c983d39675e5e3d6ec0af92d9a8910f694416737
Nathan Monroy <nathanmonroy@gmail.com>
2025-01-14 15:33:47 -0700

    first commit

---
+++ pythongittest.py
@@ -0,0 +1,12 @@
+import random
+print("Hello, this program will try to guess your age.")
+name=input("Enter your name: ")
+guess=False
+while guess==False:
+    guess=random.randint(15,30)
+    user_response=input("Are you "+str(guess)+" years old?")
+    if user_response=="y" or user_response=="Y":
+        print("{name} is {guess} years old!")
+        quit()
+    else:
+        print("Im so sorry")
