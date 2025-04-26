0754a0da13b3812b76a21c66f16e40ec0ec06d77
Nathan Monroy <nathanmonroy@gmail.com>
2025-04-26 13:19:34 -0600

    Changed age range for guesses

--- pythongittest.py
+++ pythongittest.py
@@ -3,7 +3,7 @@ print("Hello, this program will try to guess your age.")
 name=input("Enter your name: ")
 guess=False
 while guess==False:
-    guess=random.randint(15,30)
+    guess=random.randint(15,50)
     user_response=input("Are you "+str(guess)+" years old?")
     if user_response.strip()=="y" or user_response.strip()=="Y":
         print("{name} is {guess} years old!")
